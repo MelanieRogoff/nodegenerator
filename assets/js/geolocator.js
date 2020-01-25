a3bdf4f6a8be1e5ff21c7a1bf61fc2a334075f35
Melanie Rogoff <mgrogoff@gmail.com>
2020-01-24 20:03:45 -0700

    Hiding node_modules

--- assets/js/geolocator.js
+++ assets/js/geolocator.js
@@ -1,16 +1,16 @@
-// const axios = require("axios");
+const axios = require("axios");
 
-//     axios //CURRENTLY THROWING AN ERROR FOR GEOLOCATION -- LEAVING HERE TO FIX
+    axios //CURRENTLY THROWING AN ERROR FOR GEOLOCATION -- LEAVING HERE TO FIX
 
-//       .get('https://maps.googleapis.com/maps/api/geocode/json?latlng= ' + latitude + ',' + longitude + '&key=AIzaSyCiR8dLY7WEQECSzJW0P60hdvg0ReoN7z8')
-//       .then(function() {
-//           console.log("HI");
-//           let latt
+      .get('https://maps.googleapis.com/maps/api/geocode/json?latlng= ' + latitude + ',' + longitude + '&key=AIzaSyCiR8dLY7WEQECSzJW0P60hdvg0ReoN7z8')
+      .then(function() {
+          console.log("HI");
+          let latt
 
  
-// //     .catch(error => {
-// //      console.log(error)
-// //     });    
-//  });
+//     .catch(error => {
+//      console.log(error)
+//     });    
+ });
       
 
