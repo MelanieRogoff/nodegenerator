05a63dd9bd515dd87e9ddc02e93ff3162efb245d
Melanie Rogoff <mgrogoff@gmail.com>
2020-01-25 17:29:00 -0700

    Added Google Maps link, working on generateHTML js errors

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ const fs = require('fs');
 
 const pdf = require('html-pdf');
 
-const geolocator = require('./assets/js/geolocator');
-
 const generateHTML = require("./assets/js/generateHTML"); //Don't need to put .js
 
-const questions = ["What is your GitHub user name?", "What is your favorite color?", "Where are you located (city, state)?"];
+const questions = ["What is your GitHub user name?", "What is your favorite color?"];
 
 inquirer
   .prompt([
@@ -31,11 +29,6 @@ inquirer
       type: "input",
       message: questions[0],
       name: "name"
-    },
-    {
-      type: "input",
-      message: questions[2],
-      name: "questions",
     }
   ])
   .then(answers => {
@@ -46,38 +39,40 @@ inquirer
       } else {
       console.log("You chose " + answers.color + ". I love that color, too!");
       }
-      if (answers.name == '' || answers.questions == '') {
+      if (answers.name == '') {
         console.log("Oh no! You forgot to write something here! That won't do, let's try this again!");
       } else {
       console.log("Onwards!"); 
-    
-      let questions = answers.questions; //Prepping for module export
-       
+           
     pdf.create(generateHTML(answers)).toFile('./user.pdf', function(err, res) {
       
    axios //API CALL FOR GITHUB
       .get("https://api.github.com/users/" + answers.name)
       .then(function(res) {
+        console.log(res.data.name);
+        console.log("<a href='https://github.com/" + answers.name + "/>"); //User profile
         console.log("Hi!" + "<h2> My name is " + res.data.name); //username
         console.log(res.data.bio); //Bio 
         console.log("<img src='" + res.data.avatar_url + "'/>"); //bio image
         console.log(res.data.blog);   //blog
+        console.log("<a href='https://www.google.com/maps/place/" + res.data.location + "/>"); //THEN NEED TO MAKE IT A LINK TO GOOGLE MAPS
         console.log("Public Repositories: " + "<p>" + res.data.public_repos + "</p>"); // # of public repos
         console.log("Followers: " + "<p>" + res.data.followers + "</p>"); // # of followers
         console.log("Following: " + "<p>" + res.data.following + "</p>"); // # following
         console.log("GitHub Stars: " + "<p>" + res.data.starred_url + "</p>"); // # of GitHub stars
-        console.log(res.data.url); //GitHub PROFILE?
       })
       .catch(error => {
         console.log(error)
     });  
-    
+
 if (err) return console.log(err);
       console.log(res); // { filename: './user.pdf' }
-      (async () => { //This opens the PDF in a new browser via open npm
+      (async () => { //This opens PDF in new browser via open npm
         await open('./user.pdf');
       })();
    });
-  }})})
+  }})
+  answers = answers.name;
+  module.exports = answers;
+})
 
-module.exports = questions;
